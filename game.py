17ca71e0b392e0ab322fb03425fc66bf03effa01
Bibi <chichi11mythmaster@gmail.com>
2023-01-29 18:21:22 +0200

    Game logic cleaned up To Do: Unit testing

--- game.py
+++ game.py
@@ -5,97 +5,69 @@ class Game:
 
     def __init__(self):
         self.board = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
+        self.player_symbol = "X"
+        self.turn_counter = 0
+        self.play_game()
 
-    def _check_if_board_full(self):
-        full_cells = 0
-
-        for row in self.board:
-            for item in row:
-                if item != '_':
-                    full_cells += 1
-
-                if full_cells == 9:
-                    return True
-                else:
-                    return False
-
-    def _horizontal_win_check(self): #TODO parametrize by symbol using the player variable
-        x_counter = 0
-        o_counter = 0
+    def _horizontal_win_check(self, player_symbol):
+        counter = 0
         for i in range(len(self.board)):
             for item in self.board[i]:
-                if item == "X":
-                    x_counter += 1
-                elif item == "O":
-                    o_counter += 1
-            if x_counter == 3:
-                return 'We have a winner! Player 1 wins!'
+                if item == player_symbol:
+                    counter += 1
+            if counter == 3:
+                return True
             else:
-                x_counter = 0
-            if o_counter == 3:
-                return 'We have a winner! Player 2 wins!'
-            else:
-                o_counter = 0
+                counter = 0
 
-    def _vertical_win_check(self):
-        x_counter = 0
-        o_counter = 0
+    def _vertical_win_check(self, player_symbol):
+        counter = 0
         for i in range(len(self.board)):
             for j in range(len(self.board)):
-                if self.board[j][i] == "X":
-                    x_counter += 1
-                elif self.board[j][i] == "O":
-                    o_counter += 1
-            if x_counter == 3:
-                return 'We have a winner! Player 1 wins!'
+                if self.board[j][i] == player_symbol:
+                    counter += 1
+            if counter == 3:
+                return True
             else:
-                x_counter = 0
-            if o_counter == 3:
-                return 'We have a winner! Player 2 wins!'
-            else:
-                o_counter = 0
+                counter = 0
 
-    def _diagonal_win_check(self):
-        if self.board[1][1] == "X":
-            if self.board[0][0] == "X" and self.board[2][2] == "X" or \
-                    self.board[0][2] == "X" and self.board[2][0] == "X":
-                return 'We have a winner! Player 1 wins!'
-        elif self.board[1][1] == "O":
-            if self.board[0][0] == "O" and self.board[2][2] == "O" or \
-                    self.board[0][2] == "O" and self.board[2][0] == "O":
-                return 'We have a winner! Player 2 wins!'
+    def _diagonal_win_check(self, player_symbol):
+        if self.board[1][1] == player_symbol:
+            if self.board[0][0] == player_symbol and self.board[2][2] == player_symbol or \
+                    self.board[0][2] == player_symbol and self.board[2][0] == player_symbol:
+                return True
 
     def __str__(self):
         return '\n'.join(['  '.join(row) for row in self.board])
 
-    def get_random_first_player(self): # TODO use random.choice instead of randint to choose from "X" or "O"
-        return random.randint(0, 1)
+    def _get_random_first_player(self):
+        return random.choice(["X", "O"])
 
-    def swap_player_turn(self, player):
-        return "X" if player == "O" else 'O'
+    def _swap_player_turn(self, player):
+        return "X" if self.player_symbol == "O" else 'O'
 
-    def validate_user_input(self, user_input):
+    def _validate_user_input(self, user_input):
         if user_input.isnumeric() and int(user_input) in range(1, 9):
             return True
         return False
 
-    def get_row_and_col(self, user_input):
+    def _get_row_and_col(self, user_input):
         row = (int(user_input) - 1) // 3
         column = (int(user_input) - 1) % 3
         return row, column
 
-    def turn_sequence(self):
+    def play_game(self):
 
-        player = "X" if self.get_random_first_player() == 1 else 'O'
-        while True:
-            print(f"Player {player} turn")
+        self.player_symbol = self._get_random_first_player()
+        while self.turn_counter < 9:
+            print(f"Player {self.player_symbol} turn")
             print(self.__str__())
 
             user_defined_cell = input("Please select an index from 1 to 9 to place your symbol: ").strip()
 
             try:
-                self.validate_user_input(user_defined_cell)
-                row, col = self.get_row_and_col(user_defined_cell)
+                self._validate_user_input(user_defined_cell)
+                row, col = self._get_row_and_col(user_defined_cell)
             except ValueError:
                 print("Please enter a valid cell number 1 to 9!")
                 continue
@@ -104,29 +76,29 @@ class Game:
             if not self.board[row][col] == "_":
                 print("This cell is taken! Please enter new row and column indexes! ")
                 continue
-            self.board[row][col] = player
+            self.board[row][col] = self.player_symbol
 
-            # check whether win conditions are met #TODO change checks so they are not repeated
-            if self._horizontal_win_check():
-                print(self._horizontal_win_check())
-                break
-            elif self._diagonal_win_check():
-                print(self._diagonal_win_check())
-                break
-            elif self._vertical_win_check():
-                print(self._vertical_win_check())
+            # check whether win conditions are met
+            horizontal_check = self._horizontal_win_check(self.player_symbol)
+            vertical_check = self._vertical_win_check(self.player_symbol)
+            diagonal_check = self._diagonal_win_check(self.player_symbol)
+
+            if horizontal_check or vertical_check or diagonal_check:
+                print(f'We have a winner! Player {self.player_symbol} wins!')
                 break
 
-            # check whether board is full
-            if self._check_if_board_full():
-                print("Match Draw!")
-                break
+            # increment turn counter
+            self.turn_counter += 1
+
             # switch player
-            player = self.swap_player_turn(player)
+            self.player_symbol = self._swap_player_turn(self.player_symbol)
+
+        if self.turn_counter == 9:
+            print("Match Draw!")
         print()
         print(self.__str__())
 
 
 if __name__ == "__main__":
     game = Game()
-    game.turn_sequence()
+
